4b1b6b7836e9d82715eb1d56b031ab34ba54ae2e
JohannesIBK <50352679+JohannesIBK@users.noreply.github.com>
2020-07-02 23:50:14 +0200

    updateing on_error handling

--- src/main.py
+++ src/main.py
@@ -23,8 +23,6 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger = logging.getLogger('discord')
 logger.setLevel(logging.ERROR)
 logger.addHandler(handler)
-
-
 # ---------------------------------------------------------------
 
 
@@ -143,5 +141,5 @@ class Plyoox(commands.AutoShardedBot):
     async def create_redis_pool(self):
         self.redis = await aioredis.create_redis_pool('redis://localhost/')
 
-    # async def on_error(self, event_method, *args, **kwargs):
-    #     logger.error(traceback.format_exc())
+    async def on_error(self, event_method, *args, **kwargs):
+        logger.error(traceback.format_exc())
