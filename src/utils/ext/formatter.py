30b7e104960abd2e414632a7d36ac05220a80316
JohannesIBK <50352679+JohannesIBK@users.noreply.github.com>
2020-07-26 12:58:09 +0200

    adding some more infos

--- src/utils/ext/formatter.py
+++ src/utils/ext/formatter.py
@@ -7,13 +7,23 @@ class __Member:
         self.display_name = member.display_name
         self.discriminator = member.discriminator
         self.mention = member.mention
+        self.id = member.id
+        self.avatar = member.avatar_url
+
+    def __str__(self):
+        return f'{self.name}#{self.discriminator}'
 
 
 class __Guild:
     def __init__(self, guild: discord.Guild):
         self.member_count = guild.member_count
         self.name = guild.name
-        self.premium_subscribers = guild.premium_subscribers
+        self.premium_subscribers = guild.premium_subscription_count
+        self.id = guild.id
+        self.icon = guild.icon_url
+
+    def __str__(self):
+        return self.name
 
 
 def formatMessage(msg: str, user: discord.Member, lvl = None) -> str or None:
