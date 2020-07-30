c4201e9c07e9e0e2646b568d51d4da5c0f1299a2
JohannesIBK <50352679+JohannesIBK@users.noreply.github.com>
2020-07-30 16:29:55 +0200

    adding file to cmd Logging

--- src/others/logs.py
+++ src/others/logs.py
@@ -1,7 +1,9 @@
+from typing import Union
+
 import discord
 
 
-async def createCmdLog(ctx, embed):
+async def createCmdLog(ctx, embed, file = None):
     data = await ctx.bot.db.fetchrow("SELECT logchannel, logging FROM automod.config WHERE sid = $1", ctx.guild.id)
     logchannelID = data['logchannel']
     logging = data['logging']
@@ -9,12 +11,13 @@ async def createCmdLog(ctx, embed):
     if logging:
         logchannel = ctx.guild.get_channel(logchannelID)
 
-        if logchannel is not None:
-            await logchannel.send(embed=embed)
+        if logchannel is not None and ctx.me.permissions_in(logchannel).send_messages:
+            if file is not None:
+                await logchannel.send(embed=embed, file=file)
+            else:
+                await logchannel.send(embed=embed)
 
-
-async def createEmbedLog(ctx, modEmbed = None, userEmbed = None,
-                         member: discord.Member = None, ignoreNoLogging: bool = False):
+async def createEmbedLog(ctx, modEmbed = None, userEmbed = None, member: Union[discord.Member, discord.User] = None, ignoreNoLogging: bool = False):
     data = await ctx.bot.db.fetchrow("SELECT gmm, logchannel, logging FROM automod.config WHERE sid = $1", ctx.guild.id)
 
     userLog = data['gmm']
@@ -32,6 +35,9 @@ async def createEmbedLog(ctx, modEmbed = None, userEmbed = None,
                 return await member.send(embed=userEmbed)
             except discord.Forbidden:
                 return
+
+    if not isinstance(member, discord.Member):
+        return
 
     if userLog and member is not None and userEmbed is not None:
         if not ctx.guild.me.guild_permissions.is_superset(member.guild_permissions):
