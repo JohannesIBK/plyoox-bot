e83444c1af023dca799b6961eac196859d0e1ea1
JohannesIBK <50352679+JohannesIBK@users.noreply.github.com>
2020-08-11 19:53:21 +0200

    add cooldown and some other features

--- src/plugins/Commands.py
+++ src/plugins/Commands.py
@@ -1,33 +1,80 @@
+import asyncio
+import datetime
+from collections import defaultdict
+
 import discord
 from discord.ext import commands
 
 import main
-from utils.ext import checks, standards
+from utils.ext import checks, standards as std
 from utils.ext.cmds import grp
 
+
+# EVERYONE ROLE USER
 
 class Commands(commands.Cog):
     def __init__(self, bot: main.Plyoox):
         self.bot = bot
+        self.cooldowns = defaultdict()
 
     async def command_list(self, ctx):
-        userID = self.bot.user.id
-        msg = ctx.message.content.split(" ")
+        msgSplited = ctx.message.content.split(" ")[0]
+        commandName = msgSplited.replace(ctx.prefix, '').lower()
+        mentions = [ False, False, False ]
 
-        if msg[0] in [f'<@!{userID}> ', f'<@{userID}> ']:
-            try:
-                command = ctx.message.content.split(" ")[1]
-            except IndexError:
-                return
-        else:
-            command: str = msg[0].replace(ctx.prefix, '')
-
-        custom_command = await ctx.db.fetchrow("SELECT * FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, command)
-        if custom_command is None:
+        command = await self.bot.db.fetchrow("SELECT * FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, commandName)
+        if command is None:
             return
 
-        content = custom_command['content']
-        await ctx.send(content, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True, users=True))
+        if command['cooldown']:
+            current = ctx.message.created_at.replace(tzinfo=datetime.timezone.utc).timestamp()
+            if ctx.guild.id in self.cooldowns:
+                if command['name'] in self.cooldowns[ctx.guild.id]:
+                    bucket = self.cooldowns[ctx.guild.id][command['name']].get_bucket(message=ctx.message)
+                    if bucket.update_rate_limit(current=current):
+                        return await ctx.send(embed=std.getErrorEmbed('Der Command ist auf Cooldown.'))
+                else:
+                    self.cooldowns[ctx.guild.id][command['name']] = commands.CooldownMapping.from_cooldown(
+                        1, command['cooldown'], commands.BucketType.member)
+                    self.cooldowns[ctx.guild.id][command['name']].get_bucket(message=ctx.message).update_rate_limit(current=current)
+            else:
+                self.cooldowns.update({ctx.guild.id: {command['name']: commands.CooldownMapping.from_cooldown(
+                        1, command['cooldown'], commands.BucketType.member)}})
+                self.cooldowns[ctx.guild.id][command['name']].get_bucket(message=ctx.message).update_rate_limit(current=current)
+
+        if command['ignoredroles']:
+            userRoles = [ role.id for role in ctx.author.roles ]
+            if any(role in userRoles for role in command['ignoredroles']):
+                return await ctx.send(embed=std.getErrorEmbed('Du darfst diesen Command nicht verwenden.'))
+
+        if command['ignoredchannels']:
+            if ctx.channel.id in command['ignoredchannels']:
+                return await ctx.send(embed=std.getErrorEmbed('Du darfst diesen Command in diesem Channel nicht verwenden.'))
+
+        if command['role']:
+            role = ctx.guild.get_role(command['role'])
+            if role is None:
+                return
+            if role in ctx.author.roles:
+                await ctx.author.remove_roles(role)
+            else:
+                await ctx.author.add_roles(role)
+
+        if command['mentions']:
+            mentions = command['mentions']
+
+        if command['embed']:
+            embed = discord.Embed(color=std.plyoox_color, description=command['content'])
+            msg = await ctx.send(embed=embed,
+                                 allowed_mentions=discord.AllowedMentions(everyone=mentions[0], roles=mentions[1], users=mentions[2]))
+        else:
+            msg = await ctx.send(command['content'],
+                                 allowed_mentions=discord.AllowedMentions(everyone=mentions[0], roles=mentions[1], users=mentions[2]))
+
+        if command['delete']:
+            await asyncio.sleep(command['delete'])
+            await msg.delete()
+            await ctx.message.delete()
 
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
@@ -45,7 +92,6 @@ class Commands(commands.Cog):
             return
 
         prefixes = [f'<@!{self.bot.user.id}> ', f'<@{self.bot.user.id}> ', await self.bot.get(msg.guild.id, 'prefix')]
-
         if msg.content.startswith(tuple(prefixes)):
             await self.command_list(ctx)
 
@@ -63,18 +109,18 @@ class Commands(commands.Cog):
 
         if all_commands is not None:
             if len(all_commands) > 50:
-                return await ctx.send(embed=standards.getErrorEmbed('Der Server darf maximal 50 Commands besitzen.'))
+                return await ctx.send(embed=std.getErrorEmbed('Der Server darf maximal 50 Commands besitzen.'))
 
         if self.bot.get_command(name) is not None:
-            return await ctx.send(embed=standards.getErrorEmbed('Dieser Command darf nicht erstellt werden.'))
+            return await ctx.send(embed=std.getErrorEmbed('Dieser Command darf nicht erstellt werden.'))
 
         cmd = await ctx.db.fetchval("SELECT name FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, name)
 
         if cmd is not None:
-            return await ctx.send(embed=standards.getErrorEmbed('Dieser Command existiert bereits.'))
+            return await ctx.send(embed=std.getErrorEmbed('Dieser Command existiert bereits.'))
 
         await ctx.db.execute("INSERT INTO extra.commands (sid, name, content) VALUES ($1, $2, $3)", ctx.guild.id, name, content)
-        await ctx.send(embed=discord.Embed(color=standards.normal_color,
+        await ctx.send(embed=discord.Embed(color=std.normal_color,
                                            description='Der Command wurde erfolgreich hinzugefügt.'))
 
     @command_cmd.command()
@@ -84,10 +130,10 @@ class Commands(commands.Cog):
         cmd = await ctx.db.fetchval("SELECT name, content FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, name)
 
         if cmd is None:
-            return await ctx.send(embed=standards.getErrorEmbed('Der Command wurde nicht gefunden.'))
+            return await ctx.send(embed=std.getErrorEmbed('Der Command wurde nicht gefunden.'))
 
         await ctx.db.execute("UPDATE extra.commands SET content = $1 WHERE sid = $2 AND name = $3", content, ctx.guild.id, name)
-        await ctx.send(embed=discord.Embed(color=standards.normal_color,
+        await ctx.send(embed=discord.Embed(color=std.normal_color,
                                            description='Der Command wurde erfolgreich bearbeitet.'))
 
     @command_cmd.command(aliases=['delete', 'del'])
@@ -97,15 +143,15 @@ class Commands(commands.Cog):
         all_commands = await ctx.db.fetchval("SELECT name FROM extra.commands WHERE sid = $1", ctx.guild.id)
 
         if not all_commands:
-            return await ctx.send(embed=standards.getErrorEmbed('Der Server besitzt keine Commands.'))
+            return await ctx.send(embed=std.getErrorEmbed('Der Server besitzt keine Commands.'))
 
         cmd = await ctx.db.fetchval("SELECT name FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, name)
 
         if cmd is None:
-            return await ctx.send(embed=standards.getErrorEmbed('Der Command wurde nicht gefunden.'))
+            return await ctx.send(embed=std.getErrorEmbed('Der Command wurde nicht gefunden.'))
 
         await ctx.db.execute("DELETE FROM extra.commands WHERE sid = $1 AND name = $2", ctx.guild.id, name)
-        await ctx.send(embed=discord.Embed(color=standards.normal_color,
+        await ctx.send(embed=discord.Embed(color=std.normal_color,
                                            description='Der Command wurde erfolgreich entfernt.'))
 
     @command_cmd.command()
@@ -113,9 +159,9 @@ class Commands(commands.Cog):
         all_commands = await ctx.db.fetch("SELECT name FROM extra.commands WHERE sid = $1", ctx.guild.id)
 
         if not all_commands:
-            return await ctx.send(embed=standards.getErrorEmbed('Der Server besitzt keine Commands.'))
+            return await ctx.send(embed=std.getErrorEmbed('Der Server besitzt keine Commands.'))
 
-        embed = discord.Embed(color=standards.normal_color,
+        embed = discord.Embed(color=std.normal_color,
                               description=", ".join(f'{cmd["name"]}' for cmd in all_commands))
         await ctx.send(embed=embed)
 
