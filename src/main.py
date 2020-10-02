734dd29d5bc7710ef98ee419c9b006090c3c2811
JohannesIBK <gg.johannes.j@gmail.com>
2020-10-02 21:51:15 +0200

    use indents, remove redis and get prefix from cache

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 import asyncio
-import json
 import logging
+import sys
 import time
 import traceback
 from logging.handlers import RotatingFileHandler
 
 import aiohttp
-import aioredis
 import asyncpg
 import discord
+from asyncpg.pool import Pool
 from discord.ext import commands
 
-from other import db
+from utils.db.cache import BotCache
 from utils.ext import context
 
 # ---------------------------------------------------------------
+
 handler = RotatingFileHandler(filename='logs/discord.log', maxBytes=1024 * 10, encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 
@@ -40,15 +41,23 @@ cogs = [
     'plugins.SupportServer'
 ]
 
+intents = discord.Intents()
+intents.guild_messages = True
+intents.bans = True
+intents.presences = True
+intents.reactions = True
+intents.guilds = True
+
+intents.invites = False
+intents.typing = False
+intents.dm_messages = False
+intents.emojis = False
+intents.voice_states = False
+intents.integrations = False
 
 async def getPrefix(bot, msg: discord.Message):
-    prefixes = [f'<@!{bot.user.id}> ', f'<@{bot.user.id}> ']
-    if not msg.guild:
-        return prefixes
-    else:
-        prefix = await bot.get(msg.guild.id, 'prefix')
-        prefixes.append(prefix)
-    return prefixes
+    config = await bot.cache.get(msg.guild.id)
+    return config.prefix
 
 
 async def setGame(bot):
@@ -66,15 +75,17 @@ class Plyoox(commands.Bot):
         super().__init__(command_prefix=getPrefix,
                          case_insensitive=True,
                          max_messages=10000,
-                         allowed_mentions=discord.AllowedMentions(everyone=False, roles=False))
+                         allowed_mentions=discord.AllowedMentions(everyone=False, roles=False),
+                         intents=intents)
 
-        self.startTime: float = time.time()
-        self.version: str = 'v2.6.0'
-        self.owner_id: int = 263347878150406144
-        self.get_all_commands: dict = {}
+        self.startTime = time.time()
+        self.version = 'v2.7.0'
+        self.owner_id = 263347878150406144
+        self.get_all_commands = {}
         self.logger = logger
         self.remove_command("help")
         self.commandsCount = {}
+        self.cache: BotCache = BotCache(self)
 
     async def on_ready(self):
         self.gamesLoop = asyncio.create_task(setGame(self))
@@ -122,63 +133,8 @@ class Plyoox(commands.Bot):
         else:
             self.commandsCount[commandName] += 1
 
-    async def get(self, guildID: int, item: str):
-        data = await self.redis.get(guildID, encoding='utf-8')
-        if data is not None:
-            try:
-                redisData = json.loads(data)[item]
-                if item == 'noxpchannels':
-                    if redisData is None:
-                        return None
-                    return list(map(int, redisData))
-                elif item == 'noxprole':
-                    if redisData is None:
-                        return None
-                    return int(redisData)
-                return redisData
-            except KeyError:
-                pass
-
-        if item == 'prefix':
-            query = 'SELECT prefix FROM config.guild WHERE sid = $1'
-        elif item in ['noxprole', 'noxpchannels']:
-            query = 'SELECT noxprole, noxpchannels FROM config.leveling WHERE sid = $1'
-        elif item in ['words', 'state']:
-            query = 'SELECT words, state FROM automod.blacklist WHERE sid = $1'
-        elif item in ['leveling', 'automod']:
-            query = 'SELECT leveling, automod FROM config.modules WHERE sid = $1'
-        else:
-            raise ValueError(f'Item {item} in DB not found')
-
-        resp = await self.db.fetchrow(query, guildID)
-        if resp is None and item in ['prefix', 'leveling', 'automod']:
-            await db.gotAddet(self, self.get_guild(guildID))
-            resp = await self.db.fetchrow(query, guildID)
-        elif resp is None:
-            return None
-
-        if data is None:
-            await self.redis.set(guildID, json.dumps({item: resp[item]}))
-        else:
-            currentGuildData = json.loads(await self.redis.get(guildID, encoding='utf-8'))
-            if currentGuildData is None:
-                return None
-            currentGuildData[item] = resp[item]
-            await self.redis.set(guildID, json.dumps(currentGuildData))
-        return resp[item]
-
-    async def update_redis(self, guildID, data: dict):
-        currentData: dict = json.loads(await self.redis.get(guildID, encoding='utf-8'))
-        if currentData is None:
-            return await self.redis.get(guildID, "{}")
-        currentData.update(data)
-        await self.redis.set(guildID, json.dumps(currentData))
-
-    async def create_db_pool(self):
-        self.db = await asyncpg.create_pool(database='discord', user='plyoox', password='1')
-
-    async def create_redis_pool(self):
-        self.redis = await aioredis.create_redis_pool('redis://localhost/')
+    async def create_db_pool(self, port):
+        self.db: Pool = await asyncpg.create_pool(database='discord', user='plyoox', password='1', port=port)
 
     async def on_error(self, event_method, *args, **kwargs):
          logger.error(traceback.format_exc())
