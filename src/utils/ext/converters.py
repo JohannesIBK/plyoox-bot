b8aac7eb26dc74ead1dab95c7f7ad444276a5d70
JohannesIBK <gg.johannes.j@gmail.com>
2020-10-18 17:44:48 +0200

    add AdvancedMember converter

--- src/utils/ext/converters.py
+++ src/utils/ext/converters.py
@@ -72,3 +72,14 @@ class BannedMember(commands.Converter):
         if user is None:
             raise commands.BadArgument('Dieser User ist nicht gebannt.')
         return user
+
+class AdvancedMember(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            return await commands.UserConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                user_id = int(argument, base=10)
+                return await ctx.bot.fetch_user(user_id)
+            except (ValueError, discord.NotFound):
+                raise commands.UserNotFound
