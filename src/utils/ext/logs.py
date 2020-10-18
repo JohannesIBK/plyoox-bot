fbd966aa441bd590e11984302a3d7977fb88b50b
JohannesIBK <gg.johannes.j@gmail.com>
2020-10-18 17:46:33 +0200

    rewrite log handler

--- src/utils/ext/logs.py
+++ src/utils/ext/logs.py
@@ -1,47 +1,40 @@
-from typing import Union
-
 import discord
+
+from utils.ext.context import Context
 
 
 async def createCmdLog(ctx, embed, file = None):
-    data = await ctx.bot.db.fetchrow("SELECT logchannel, logging FROM automod.config WHERE sid = $1", ctx.guild.id)
-    if data is None:
-        return
+    data = await ctx.cache.get(ctx.guild.id)
+    config = data.automod.config
 
-    if data['logging']:
-        logchannel = ctx.guild.get_channel(data['logchannel'])
-
-        if logchannel is not None and ctx.me.permissions_in(logchannel).send_messages:
+    if data.modules.logging and config.logchannel is not None:
+        if ctx.me.permissions_in(config.logchannel).send_messages:
             if file is not None:
-                await logchannel.send(embed=embed, file=file)
+                await config.logchannel.send(embed=embed, file=file)
             else:
-                await logchannel.send(embed=embed)
+                await config.logchannel.send(embed=embed)
 
-async def createEmbedLog(ctx, modEmbed = None, userEmbed = None, member: Union[discord.Member, discord.User] = None, ignoreNoLogging = False, ignoreMMSG = False):
-    data = await ctx.bot.db.fetchrow("SELECT gmm, logchannel, logging FROM automod.config WHERE sid = $1", ctx.guild.id)
-    if data is None:
+async def createLog(ctx: Context, *, user: discord.Member = None, mEmbed = None, uEmbed = None, automod = False):
+    data = await ctx.cache.get(ctx.guild.id)
+    config = data.automod.config
+
+    if data.modules.logging or automod:
+        if config.logchannel is not None and mEmbed is not None:
+            if ctx.me.permissions_in(config.logchannel).send_messages:
+                await config.logchannel.send(embed=mEmbed)
+
+    if user is None:
         return
 
-    if (data['logging'] or ignoreNoLogging) and modEmbed is not None:
-        logchannel = ctx.guild.get_channel(data['logchannel'])
-        if logchannel is not None:
-            if ctx.me.permissions_in(logchannel).send_messages:
-                await logchannel.send(embed=modEmbed)
+    if not isinstance(user, discord.Member):
+        return
 
-        if ignoreMMSG:
-            try:
-                return await member.send(embed=userEmbed)
-            except discord.Forbidden:
+    if config.gmm or automod:
+        if uEmbed is not None:
+            if not ctx.guild.me.guild_permissions.is_superset(user.guild_permissions):
                 return
 
-    if not isinstance(member, discord.Member):
-        return
-
-    if data['gmm'] and member is not None and userEmbed is not None:
-        if not ctx.guild.me.guild_permissions.is_superset(member.guild_permissions):
-            return
-
-        try:
-            return await member.send(embed=userEmbed)
-        except discord.Forbidden:
-            return
+            try:
+                await user.send(embed=uEmbed)
+            except (discord.Forbidden, discord.HTTPException):
+                return
