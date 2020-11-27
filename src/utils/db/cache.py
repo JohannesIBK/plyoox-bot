cf1bf48273738b1d4454ab1660cea998f52698bd
JohannesIBK <gg.johannes.j@gmail.com>
2020-11-27 18:51:56 +0100

    add language to cache and some improvements

--- src/utils/db/cache.py
+++ src/utils/db/cache.py
@@ -1,21 +1,26 @@
 import asyncio
-#
+
 from . import leveling, modules, automod
 
 
 class GuildConfig:
-    __slots__ = ('_leveling', '_automod', '_modules', '_prefix', 'sid', 'bot')
+    __slots__ = ('_language', '_leveling', '_automod', '_modules', '_prefix', 'sid', 'bot')
 
     _leveling: leveling.Leveling
     _automod: automod.Automod
     _modules: modules.Modules
     _prefix: str
+    _language: str
     sid: int
 
     def __init__(self, bot, sid, records):
         self.sid = sid
         self.bot = bot
-        self._prefix = records[0]
+
+        if records[0] is not None:
+            self._prefix = records[0]["prefix"]
+            self._language = records[0]["lang"]
+
         self._automod = automod.Automod(self.bot, self.sid, records[1])
         self._leveling = leveling.Leveling(self.bot, self.sid, records[2])
         self._modules = modules.Modules(self.bot, self.sid, records[2])
@@ -30,6 +35,14 @@ class GuildConfig:
     @prefix.setter
     def prefix(self, prefix):
         self._prefix = prefix
+
+    @property
+    def lang(self) -> str:
+        return self._language or "en"
+
+    @lang.setter
+    def lang(self, lang):
+        self._language = lang
 
     @property
     def automod(self) -> automod.Automod:
@@ -59,11 +72,14 @@ class BotCache:
 
             # one request would take 2 seconds fml
             # please help
-            part1 = await self.bot.db.fetchrow('SELECT sid, invitestate, invitepoints, invitewhitelist, invitepatner FROM automod.automod WHERE sid = $1', sid)
-            part2 = await self.bot.db.fetchrow('SELECT linksstate, linkspoints, linkswhitelist, linksiswhitelist, linkslinks, mentionseveryone FROM automod.automod WHERE sid = $1', sid)
-            part3 = await self.bot.db.fetchrow('SELECT mentionsstate, mentionspoints, mentionswhitelist, mentionscount, capspoints FROM automod.automod WHERE sid = $1', sid)
-            part4 = await self.bot.db.fetchrow('SELECT capswhitelist, blacklistpoints, blacklistwhitelist, blacklistwords, capsstate, blackliststate FROM automod.automod WHERE sid = $1', sid)
-            automodConfig = await self.bot.db.fetchrow('SELECT c.* FROM automod.config c WHERE c.sid = $1', sid)
+            part1, part2, part3, part4, automodConfig, settings = await asyncio.gather(*[
+                self.bot.db.fetchrow('SELECT sid, invitestate, invitepoints, invitewhitelist, invitepatner FROM automod.automod WHERE sid = $1', sid),
+                self.bot.db.fetchrow('SELECT linksstate, linkspoints, linkswhitelist, linksiswhitelist, linkslinks, mentionseveryone FROM automod.automod WHERE sid = $1', sid),
+                self.bot.db.fetchrow('SELECT mentionsstate, mentionspoints, mentionswhitelist, mentionscount, capspoints FROM automod.automod WHERE sid = $1', sid),
+                self.bot.db.fetchrow('SELECT capswhitelist, blacklistpoints, blacklistwhitelist, blacklistwords, capsstate, blackliststate FROM automod.automod WHERE sid = $1', sid),
+                self.bot.db.fetchrow('SELECT c.* FROM automod.config c WHERE c.sid = $1', sid),
+                self.bot.db.fetchrow('SELECT prefix, lang FROM config.guild WHERE sid = $1', sid)
+            ])
 
             automodRecord = dict(automodConfig) if automodConfig else {}
             if part1:
@@ -72,16 +88,16 @@ class BotCache:
                 automodRecord.update(dict(part3))
                 automodRecord.update(dict(part4))
 
-
             records = await self.bot.db.fetchrow('SELECT * FROM config.leveling l FULL JOIN config.modules m ON l.sid = m.sid WHERE l.sid = $1', sid)
 
-            cache = GuildConfig(self.bot, sid, ['-', automodRecord or None, records])
+            cache = GuildConfig(self.bot, sid, [settings, automodRecord or None, records])
             self.cache.update({ sid: cache })
+
         return cache
 
     async def set(self, sid):
         records = await asyncio.gather(*[
-            self.bot.db.fetchval('SELECT prefix FROM config.guild WHERE sid = $1', sid),
+            self.bot.db.fetchrow('SELECT prefix, lang FROM config.guild WHERE sid = $1', sid),
             self.bot.db.fetchrow('SELECT c.*, a.* FROM automod.automod a FULL JOIN automod.config c ON a.sid = c.sid WHERE c.sid = $1', sid),
             self.bot.db.fetchrow('SELECT * FROM config.leveling l FULL JOIN config.modules m ON l.sid = m.sid WHERE l.sid = $1', sid),
         ])
