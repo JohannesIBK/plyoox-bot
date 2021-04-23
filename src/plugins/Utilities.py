d2ff0765e0d2be2f7b1ba851de1550464b731702
JohannesIBK <gg.johannes.j@gmail.com>
2021-04-23 09:12:47 +0200

    use datetime.utcnow() instead of datetime.now()

--- src/plugins/Utilities.py
+++ src/plugins/Utilities.py
@@ -23,7 +23,7 @@ class Utilities(commands.Cog):
 
         embed.title = lang["someone.embed.title"]
         embed.description = std.quote(
-            lang["someone.embed.description"].format(d=str((datetime.datetime.now() - user.joined_at).days), u=user))
+            lang["someone.embed.description"].format(d=str((datetime.datetime.utcnow() - user.joined_at).days), u=user))
         embed.set_thumbnail(url=user.avatar_url)
         embed.set_footer(icon_url=ctx.author.avatar_url, text=f'Requested by {ctx.author}')
         await ctx.reply(embed=embed)
